49cd17d0eac6b1fbd48e242cdad844ec255f099c
春风化雨细如丝 <indiff@126.com>
2025-10-27 10:45:30 +0800

    Update build-llvm.sh

--- build-llvm.sh
+++ build-llvm.sh
@@ -3,6 +3,5 @@
 set -xe
 git clone --filter=blob:none https://github.com/indiff/bash-shell.git --depth=1
 cd "bash-shell/LLVM&Clang Installer/20.1/"
-
-
+chmod +x ./*.sh
 env CC=/opt/gcc-indiff/bin/gcc CXX=/opt/gcc-indiff/bin/g++ ./installer-bootstrap.sh -p /workspace/gcc-x86
